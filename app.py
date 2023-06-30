f7a2847640bdabada6bd1071a5d4018f0265a64f
sesychev <sergeysychou@gmail.com>
2023-06-30 11:38:08 +0300

    feat: add icons

--- app.py
+++ app.py
@@ -27,9 +27,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
+#@app.route('/about')
+#def about():
+#    return render_template('about.html')
 
 @app.route('/awards')
 def awards():
